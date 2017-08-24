fdf7d2946df3045110fa7ac92183d31f5a7ecf26
psilva.igor <psilva.igor@gmail.com>
2017-08-24 15:02:12 -0400

    Consumindo mongoDB remoto

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
+//module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect('mongodb://app:12345@ds159033.mlab.com:59033/todo')
